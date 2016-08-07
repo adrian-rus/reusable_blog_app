352553cc72b634361f4822ef5c98882c1962b8c7
Dragos Leo <adrian.rus@live.com>
2016-08-07 11:33:30 +0300

    upgraded to version 1.0.4

--- reusable_blog/models.py
+++ reusable_blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     views = models.IntegerField(default=0)  # record how many times a post has been seen
     tag = models.CharField(max_length=30, blank=True, null=True)
-    image = models.ImageField(upload_to="images", blank=True, null=True)
+    # image = models.ImageField(upload_to="images", blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
